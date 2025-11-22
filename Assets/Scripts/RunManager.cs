ebb9825239c9c072fa949317e701aac98b75ea34
Scott Dagen <scottdagen@gmail.com>
2025-11-22 14:15:03 -0500

    [Untested] Enemy selection

--- Assets/Scripts/RunManager.cs
+++ Assets/Scripts/RunManager.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class RunManager : MonoBehaviour
 {
@@ -9,9 +11,28 @@ public class RunManager : MonoBehaviour
 
 	[SerializeField]
 	private List<RunEvent> _runFormat;
+	public List<RunEvent> RunFormat => _runFormat;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+	[Header("Do not modify this! This shows what has been selected so far")]
+	public List<Vector2Int> _currentRun;
+
+	public static RunManager INSTANCE;
+
+	private void Awake()
+	{
+		// set up singleton
+
+		if (INSTANCE != null && INSTANCE != this)
+		{
+			Destroy(gameObject);
+			return;
+		}
+
+		INSTANCE = this;
+	}
+
+	// Start is called once before the first execution of Update after the MonoBehaviour is created
+	void Start()
     {
         
     }
@@ -47,6 +68,48 @@ public class RunManager : MonoBehaviour
 			}
 		}
 	}
+
+
+	public void SelectNextEvent(int option = 0)
+	{
+		if (_currentRun.Count >= _runFormat.Count)
+			return;
+
+		int eventIndex = _currentRun.Count;
+
+		RunEvent nextEvent = _runFormat[eventIndex];
+
+		if (nextEvent.EventKinds.Count == 0)
+			return;
+
+		if (nextEvent.EventKinds.Count < option)
+		{
+			Debug.LogWarning("Invalid option for RunEvent!");
+			option = 0;
+		}
+
+		EncounterPoolKind poolKind = nextEvent.EventKinds[option];
+
+		if (poolKind == EncounterPoolKind.Shop)
+		{
+			_currentRun.Add(new Vector2Int(option, -1));
+			return;
+		}
+
+		if (poolKind == EncounterPoolKind.All)
+		{
+			Debug.LogError("We don't support EncounterPoolKind.All yet");
+			_currentRun.Add(new Vector2Int(option, -1));
+			return;
+		}
+
+		EncounterPool pool = _pools.Find(pool => pool.PoolKind == nextEvent.EventKinds[option]);
+
+		_currentRun.Add(new Vector2Int(option, pool.GetWeightedIndex(eventIndex - 1, _currentRun[eventIndex - 1])));
+	}
+
+	public RunEvent Event(int index) => _runFormat[index];
+	public EncounterPool Pool(EncounterPoolKind kind) => _pools.Find(pool => pool.PoolKind == kind);
 }
 
 [Serializable]
@@ -54,6 +117,8 @@ public class RunEvent
 {
 	[SerializeField]
 	private List<EncounterPoolKind> _eventKinds;
+
+	public List<EncounterPoolKind> EventKinds => _eventKinds;
 }
 
 public enum EncounterPoolKind
@@ -89,9 +154,9 @@ public class EncounterPool
 
 	public enum RepeatKind
 	{
-		Never,
+		Allowed,
 		No_Consecutive,
-		Allowed
+		Never
 	}
 
 	[SerializeField]
@@ -108,5 +173,61 @@ public class EncounterPool
 		private GameObject _prefab;
 		[SerializeField, Min(0.1f)]
 		private float _weight = 1.0f;
+		public float Weight => _weight;
+	}
+
+	internal int GetWeightedIndex(int lastRunIndex, Vector2Int lastOption)
+	{
+		if (_canRepeat == RepeatKind.No_Consecutive)
+		{
+			RunEvent lastEvent = RunManager.INSTANCE.Event(lastRunIndex);
+			EncounterPool lastPool = RunManager.INSTANCE.Pool(lastEvent.EventKinds[lastOption[0]]);
+
+			if (lastPool == this)
+			{
+				float sumNoRepeat = 0;
+
+				for (int i = 0; i < _entries.Length; i++)
+				{
+					if (i == lastOption[1])
+						continue;
+
+					sumNoRepeat += _entries[i].Weight;
+				}
+
+				float randNoRepeat = UnityEngine.Random.Range(0.0f, 1.0f) * sumNoRepeat; // long-term we should have a centralized RNG so we can have consistent test cases.
+
+				for (int i = 0; i < _entries.Length; i++)
+				{
+					if (i == lastOption[1])
+						continue;
+
+					if (randNoRepeat < _entries[i].Weight)
+						return i;
+
+					randNoRepeat -= _entries[i].Weight;
+				}
+
+				return 0;
+			}
+		}
+
+		if (_canRepeat == RepeatKind.Never)
+		{
+			throw new NotSupportedException("We do not currently support encounter pools that can never repeat an entry. If/when we run into cases where this is needed, we'll add it");
+		}
+
+		float sum = _entries.Sum(entry => entry.Weight);
+		float rand = UnityEngine.Random.Range(0.0f, 1.0f) * sum; // long-term we should have a centralized RNG so we can have consistent test cases.
+
+		for (int i = 0; i < _entries.Length; i++)
+		{
+			if (rand < _entries[i].Weight)
+				return i;
+
+			rand -= _entries[i].Weight;
+		}
+
+		return 0;
 	}
 }
