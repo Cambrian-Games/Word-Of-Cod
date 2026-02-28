6e7b8ac2b0aa4fcb81a7c96573db1aa21222de4a
Connor Breen <conchors@gmail.com>
2026-02-28 12:42:28 -0500

    You can drag, but it points offset...

---
+++ Assets/Scripts/UI/Compass.cs
@@ -0,0 +1,55 @@
+using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+public class Compass : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void IBeginDragHandler.OnBeginDrag(PointerEventData eventData)
+    {
+        UpdateNeedle(eventData);
+        Debug.Log("mouse Down");
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        //Debug.Log("drag");
+        UpdateNeedle(eventData);
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        UpdateNeedle(eventData);
+        Debug.Log("mouse up");
+    }
+    
+    private void UpdateNeedle(PointerEventData eventData)
+    {
+        Vector2 screenpos = Camera.main.WorldToScreenPoint(transform.position);
+        ////Debug.Log("update needle");
+        //Vector2 posDiff = eventData.position - screenpos;
+        //posDiff.Normalize();
+        //Debug.Log(posDiff);
+        //float angle = Vector3.Angle(Input.mousePosition, screenpos);
+        //Debug.Log(angle);
+        //transform.eulerAngles= new Vector3(0, 0, angle);
+        //transform.Rotate(new Vector3(0,0,1), angle);
+        //transform.LookAt(eventData.position);
+        Vector2 mouseScreenPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Debug.Log("moustPos: " + eventData.position + "\nneedlepos: " + transform.position);
+        Vector2 difference = eventData.position - new Vector2(transform.position.x, transform.position.y);
+        float rotationZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0.0f, 0.0f, rotationZ);
+    }
+    
+}
