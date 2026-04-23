13927b5d77f3800aa0fc143062dab4d6f25051aa
Scott Dagen <scottdagen@gmail.com>
2026-04-23 10:37:57 -0400

    Vastly cleaned up letter generation

--- Assets/Scripts/CharacterWeights.cs
+++ Assets/Scripts/CharacterWeights.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Linq;
 using UnityEngine;
 
@@ -28,270 +29,175 @@ public class CharacterWeights : ScriptableObject
 
 	private static readonly char[] VOWELS = { 'A', 'E', 'I', 'O', 'U' };
 
-	public char[] RandomChars(int count, BoardState state = null)
+
+
+	public char[] FreshBoard(BoardState state)
 	{
-		char[] newChars = new char[count];
-		int charIndex = 0;
+		Vector2Int dims = state._layout.Dims();
+		char[] newChars = new char[dims.x * dims.y];
 
-		if (state == null)
+		byte[] charCounts = new byte[26];
+
+		for (int i = 0; i < newChars.Length; i++)
 		{
-			for (; charIndex < count; charIndex++)
-			{
-				newChars[charIndex] = RandomLetter();
-			}
-			return newChars;
-		}
-		
-		int vowelCount = 0;
-		int playableTileCount = 0;
-
-		foreach (Vector2Int coord in new Vector2IntIterator(state._layout.BottomRight()))
-		{
-			switch (state[coord])
-			{
-				case 'A':
-				case 'E':
-				case 'I':
-				case 'O':
-				case 'U':
-					vowelCount++;
-					break;
-			}
-
-			switch (state._layout[coord])
-			{
-				case CellKind.Standard:
-					playableTileCount++;
-					break;
-			}
+			newChars[i] = RandomLetter(ref charCounts);
+			charCounts[newChars[i] - 'A']++;
 		}
 
-		// generate characters
+		EnforceVowelMinimums(ref newChars, newChars.Length, ref charCounts);
 
-		for (; charIndex < count; charIndex++)
-		{
-			//if (((float)vowelCount / playableTileCount) < _minVowelRate)
-			//{
-			//	newChars[charIndex] = RandomVowel(state, newChars, charIndex);
-			//	vowelCount++;
-			//}
-			//else
-			//{
-				newChars[charIndex] = RandomLetter(state, newChars, charIndex);
-
-				switch (newChars[charIndex])
-				{
-					case 'A':
-					case 'E':
-					case 'I':
-					case 'O':
-					case 'U':
-						vowelCount++;
-						break;
-				}
-			//}
-		}
-
-		// check characters in board, if total vowels <= _minVowelRate, replace first 5 consonants with vowels.
-		//  how do we check for decay when replacing letters???
-
-		// shuffle characters
-
-		for (charIndex = count; charIndex > 1; charIndex--)
-		{
-			int rand = Random.Range(0, charIndex); // charIndex is excluded
-
-			if (rand == charIndex - 1)
-				continue;
-
-			(newChars[rand], newChars[charIndex - 1]) = (newChars[charIndex - 1], newChars[rand]);
-		}
+		ShuffleChars(ref newChars);
 
 		return newChars;
 	}
 
-	public char RandomChar(BoardState state = null)
+	internal char[] RandomChars(int count, BoardState state)
 	{
-		return RandomChars(1, state)[0];
+		Vector2Int bottomRight = state._layout.BottomRight();
+
+		byte[] charCounts = new byte[26];
+		int charsInBoardState = 0;
+
+		foreach (Vector2Int coord in new Vector2IntIterator(bottomRight))
+		{
+			if (state[coord] != ' ')
+			{
+				charCounts[state[coord] - 'A']++;
+				charsInBoardState++;
+			}
+		}
+
+		Debug.Assert(charsInBoardState + count == (bottomRight.x + 1) * (bottomRight.y + 1));
+
+		if (charsInBoardState == 0)
+		{
+			return FreshBoard(state);
+		}
+
+		char[] newChars = new char[count];
+
+		for (int i = 0; i < newChars.Length; i++)
+		{
+			newChars[i] = RandomLetter(ref charCounts);
+			charCounts[newChars[i] - 'A']++;
+		}
+
+		// if we want to enforce vowel minimum here, we can
+		// EnforceVowelMinimums(ref newChars, charsInBoardState + count, ref charCounts);
+
+		return newChars;
 	}
 
-	public char RandomLetter(BoardState state = null, char[] previouslyAddedChars = null, int numPreviouslyAddedChars = -1)
+	public char RandomLetter(ref byte[] charCounts)
 	{
-		if (state == null)
+		float[] modifiedWeights = GetCurrentModifiedWeights(ref charCounts);
+
+		float sum = modifiedWeights.Sum();
+		float rand = UnityEngine.Random.Range(0.0f, 1.0f) * sum;
+
+		int charIter = 0;
+
+		while (rand > modifiedWeights[charIter])
 		{
-			float sum = _weights.Sum();
-
-			float rand = Random.Range(0.0f, 1.0f) * sum; // long-term we should have a centralized RNG so we can have consistent test cases.
-
-			for (int charIter = 0; charIter < 26; charIter++)
-			{
-				if (rand < _weights[charIter])
-					return (char)('A' + charIter);
-
-				rand -= _weights[charIter];
-			}
+			rand -= modifiedWeights[charIter];
+			charIter++;
 		}
 
-		else
-		{
-			float[] modifiedWeights = GetCurrentModifiedWeights(state, previouslyAddedChars, numPreviouslyAddedChars);
-
-			float sum = modifiedWeights.Sum();
-			float rand = Random.Range(0.0f, 1.0f) * sum;
-
-			for (int charIter = 0; charIter < 26; charIter++)
-			{
-				if (rand < modifiedWeights[charIter])
-					return (char)('A' + charIter);
-
-				rand -= modifiedWeights[charIter];
-			}
-		}
-
-
-		return 'Z';
+		return (char) ('A' + charIter);
 	}
 
-	private float[] GetCurrentModifiedWeights(BoardState state, char[] previouslyAddedChars, int numPreviouslyAddedChars)
+	public char RandomVowel(ref byte[] charCounts)
 	{
-		float[] result = new float[26];
+		float[] modifiedWeights = GetCurrentModifiedWeights(ref charCounts);
 
-		for (int charIter = 0; charIter < 26; charIter ++)
+		float sum = VOWELS.Sum(vowel => modifiedWeights[vowel - 'A']);
+		float rand = UnityEngine.Random.Range(0.0f, 1.0f) * sum;
+
+		int vowelIter = 0;
+
+		while (rand > modifiedWeights[VOWELS[vowelIter]])
 		{
-			char c = (char) ('A' + charIter);
-			switch (c)
-			{
-				case 'A':
-				case 'E':
-				case 'I':
-				case 'O':
-				case 'U':
-					int numPresent = state.CountLetter(c);
-
-					for (int i = 0; i < numPreviouslyAddedChars; i++)
-					{
-						if (previouslyAddedChars[i] == c)
-							numPresent++;
-					}
-
-					if (numPresent >= _defaultZeroThreshold)
-					{
-						result[charIter] = 0;
-						break;
-					}
-
-					if (numPresent > _defaultDecayThreshold && !_reducePercent)
-					{
-						// a(1-u) + b(u) = x
-						// a = _defaultDecayThreshold
-						// b = _defaultZeroThreshold - 1
-						// x = numPresent
-
-						// u = (a-x) / (a-b)
-
-						float u = (_defaultDecayThreshold - numPresent) / (float)(_defaultDecayThreshold - (_defaultZeroThreshold - 1));
-						result[charIter] = _weights[charIter] * _vowelCurve.Evaluate(u);
-						break;
-					}
-
-					result[charIter] = _weights[charIter];
-					break;
-
-				default:
-					result[charIter] = _weights[charIter];
-					break;
-			}
+			rand -= modifiedWeights[VOWELS[vowelIter]];
+			vowelIter++;
 		}
 
-		if (_reducePercent && false)
-		{
-			float totalDefaultWeight = _weights.Sum();
-
-			float[] percentScalars = new float[VOWELS.Length];
-			float[] defaultPercents = new float[VOWELS.Length];
-
-			for (int vowelIter = 0; vowelIter < VOWELS.Length; vowelIter++)
-			{
-				defaultPercents[vowelIter] = _weights[VOWELS[vowelIter] - 'A'] / totalDefaultWeight;
-			}
-
-			for (int vowelIter = 0; vowelIter < VOWELS.Length; vowelIter++)
-			{
-				int numPresent = state.CountLetter(VOWELS[vowelIter]);
-
-				for (int i = 0; i < numPreviouslyAddedChars; i++)
-				{
-					if (previouslyAddedChars[i] == VOWELS[vowelIter])
-						numPresent++;
-				}
-
-				if (numPresent >= _defaultZeroThreshold)
-				{
-					percentScalars[vowelIter] = 0;
-					continue;
-				}
-
-				if (numPresent > _defaultDecayThreshold && !_reducePercent)
-				{
-					// a(1-u) + b(u) = x
-					// a = _defaultDecayThreshold
-					// b = _defaultZeroThreshold - 1
-					// x = numPresent
-
-					// u = (a-x) / (a-b)
-
-					float u = (_defaultDecayThreshold - numPresent) / (float)(_defaultDecayThreshold - (_defaultZeroThreshold - 1));
-					percentScalars[vowelIter] = _vowelCurve.Evaluate(u);
-					continue;
-				}
-
-				percentScalars[vowelIter] = 1.0f;
-			}
-
-			// how on earth do we do this math...
-			// target percents are known, it's the defaultPercent * the percentScalar,
-			// but reducing the percent of any vowel necessarily increases the percents of the others
-		}
-
-		return result;
+		return (char)('A' + VOWELS[vowelIter]);
 	}
 
-	public char RandomVowel(BoardState state = null, char[] previouslyAddedChars = null, int numPreviouslyAddedChars = -1)
+	public void EnforceVowelMinimums(ref char[] newChars, int totalChars, ref byte[] charCounts)
 	{
-		if (state == null)
+		int vowelCount = 0;
+		foreach (char vowel in VOWELS)
 		{
-			float sum = VOWELS.Select(vowel => _weights[vowel - 'A']).Sum();
+			vowelCount += charCounts[vowel - 'A'];
+		}
+		
+		float vowelRate = vowelCount / (float)totalChars;
 
-			float rand = Random.Range(0.0f, 1.0f) * sum;
+		int charIter = 0;
 
-			for (int charIter = 0; charIter < VOWELS.Count(); charIter++)
+		while (vowelRate < _minVowelRate && charIter < newChars.Length)
+		{
+			if (VOWELS.Contains(newChars[charIter]))
+				continue;
+
+			// if the char is not a vowel, subtract it from the char counts and get a vowel
+
+			charCounts[newChars[charIter]]--;
+
+			newChars[charIter] = RandomVowel(ref charCounts);
+
+			charCounts[newChars[charIter]]++;
+
+			vowelCount++;
+			vowelRate = vowelCount / (float)newChars.Length;
+			charIter++;
+		}
+	}
+
+	public void ShuffleChars(ref char[] chars)
+	{
+		for (int charIndex = chars.Length; charIndex > 1; charIndex--)
+		{
+			int rand = UnityEngine.Random.Range(0, charIndex); // charIndex is excluded
+
+			if (rand == charIndex - 1)
+				continue;
+
+			(chars[rand], chars[charIndex - 1]) = (chars[charIndex - 1], chars[rand]);
+		}
+	}
+
+	private float[] GetCurrentModifiedWeights(ref byte[] charCounts)
+	{
+		float[] newWeights = new float[_weights.Length];
+		Array.Copy(_weights, newWeights, _weights.Length);
+
+		foreach (char vowel in VOWELS)
+		{
+			byte charCount = charCounts[vowel - 'A'];
+
+			if (charCount >= _defaultZeroThreshold)
 			{
-				if (rand < _weights[VOWELS[charIter] - 'A'])
-					return VOWELS[charIter];
-
-				rand -= _weights[VOWELS[charIter] - 'A'];
+				newWeights[vowel - 'A'] = 0;
+				continue;
 			}
 
-			return 'U';
-		}
-		else
-		{
-			float[] modifiedWeights = GetCurrentModifiedWeights(state, previouslyAddedChars, numPreviouslyAddedChars);
-
-			float sum = VOWELS.Select(vowel => modifiedWeights[vowel - 'A']).Sum();
-
-			float rand = Random.Range(0.0f, 1.0f) * sum;
-
-			for (int charIter = 0; charIter < VOWELS.Count(); charIter++)
+			if (charCount > _defaultDecayThreshold)
 			{
-				if (rand < modifiedWeights[VOWELS[charIter] - 'A'])
-					return VOWELS[charIter];
+				// a(1-u) + b(u) = x
+				// a = _defaultDecayThreshold
+				// b = _defaultZeroThreshold - 1
+				// x = numPresent
 
-				rand -= modifiedWeights[VOWELS[charIter] - 'A'];
+				// u = (a-x) / (a-b)
+
+				float u = (_defaultDecayThreshold - charCount) / (float)(_defaultDecayThreshold - (_defaultZeroThreshold - 1));
+				newWeights[vowel - 'A'] *= _vowelCurve.Evaluate(u);
+				continue;
 			}
-
-			return 'U';
 		}
 
+		return newWeights;
 	}
 }
