cb66b5fde301abaddd488083b9f2dead88d4f34a
Scott Dagen <scottdagen@gmail.com>
2026-08-27 20:31:09 -0400

    Start of backend

--- Assets/Scripts/UI/LetterWeightMenu.cs
+++ Assets/Scripts/UI/LetterWeightMenu.cs
@@ -3,16 +3,22 @@ using UnityEngine;
 
 public class LetterWeightMenu : MonoBehaviour
 {
+#if UNITY_EDITOR
     [SerializeField]
     private LetterWeightRow _letterWeightRowPrefab;
+
     [SerializeField]
     private GameObject _letterWeightParent;
+#endif
 
     [SerializeField]
     private List<LetterWeightRow> _letterWeightRows = new List<LetterWeightRow>();
 
+    public CharacterSet _charset;
+
     private void OnEnable()
     {
+#if UNITY_EDITOR
         if (_letterWeightRows.Count > 26)
         {
             _letterWeightRows.RemoveRange(26, _letterWeightRows.Count - 26);
@@ -21,8 +27,15 @@ public class LetterWeightMenu : MonoBehaviour
         {
             _letterWeightRows.Add(Instantiate(_letterWeightRowPrefab, _letterWeightParent.transform));
         }
+        
+        for (int i = 0; i < _letterWeightRows.Count; i++)
+        {
+            _letterWeightRows[i].BaseWeightText.text = $"{BoardConfig.INSTANCE.Weights._weights[i]}";
+        }
+#endif
     }
 
+#if UNITY_EDITOR
     [ContextMenu("Fix")]
     public void FixNamesAndPositions()
     {
@@ -30,6 +43,8 @@ public class LetterWeightMenu : MonoBehaviour
         {
             _letterWeightRows[i].name = "Letter Weight (" + (char)('A' + i) + ")";
             _letterWeightRows[i].transform.localPosition = new Vector3(_letterWeightRows[i].transform.localPosition.x, -10 - (20 * i), _letterWeightRows[i].transform.localPosition.z);
+            _letterWeightRows[i].LetterIcon.sprite = _charset._letterSprites[i];
         }
     }
+#endif
 }
--- Assets/Scripts/UI/LetterWeightRow.cs
+++ Assets/Scripts/UI/LetterWeightRow.cs
@@ -1,16 +1,36 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LetterWeightRow : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    #region UI Elements
+    [SerializeField]
+    private Image _letterIcon;
+    public Image LetterIcon => _letterIcon;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [SerializeField]
+    private TMP_Text _baseWeightText;
+    public TMP_Text BaseWeightText => _baseWeightText;
+
+    [SerializeField]
+    private Button _minusButton, _plusButton;
+    public Button MinusButton => _minusButton;
+    public Button PlusButton => _plusButton;
+
+    [SerializeField]
+    private TMP_Text _playerTweakText;
+    public TMP_Text PlayerTweakText => _playerTweakText;
+
+    [SerializeField]
+    private TMP_Text _finalWeightText;
+    public TMP_Text FinalWeightText => _finalWeightText;
+    #endregion
+
+#if UNITY_EDITOR
+    public char _letter;
+#else
+    [SerializeField, HideInInspector]
+    private char _letter;
+#endif
 }
